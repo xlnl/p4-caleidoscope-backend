b40c585684ad44f21116966b08ab3bcb55b65e79
xlnl <lamanhnle@gmail.com>
2021-03-06 17:32:57 -0500

    muser session works now, just got to make sure connection is OK on deployed app

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, g, session, make_response
-from flask_cors import CORS, logging
+from flask_cors import CORS, logging, cross_origin
 from flask_login import LoginManager
 
 import os
@@ -21,10 +21,17 @@ app.config.from_pyfile('config.py')
 
 ############ vv "MIDDLEWARE" METHODS vv ##############
 
-CORS(app, origins=['http://localhost:3000','https://caleidoscope.herokuapp.com'], supports_credentials=True) 
+CORS(app, origins=['http://localhost:3000','https://caleidoscope.herokuapp.com/', 'https://caleidscope-api.herokuapp.com'], supports_credentials=True) 
 
+app.register_blueprint(person, url_prefix='/api/v1/user')
+app.register_blueprint(note, url_prefix='/api/v1/note')
+app.register_blueprint(event, url_prefix='/api/v1/event')
 # express equivalent = app.use('/api/v1/note')
+CORS(person)
+CORS(event)
+CORS(note)
 
+logging.getLogger('flask_cors').level = logging.DEBUG
 login_manager = LoginManager() # instantiating a new LoginManager in an app 
 login_manager.init_app(app)
 
@@ -50,20 +57,14 @@ def before_request():
 @app.after_request
 def after_request(response):
     app.config.update(SESSION_COOKIE_SAMESITE="None", SESSION_COOKIE_SECURE=True)
-    response.headers.add("Set-Cookie", f"my_cookie='a cookie'; Secure; SameSite=None;","Access-Control-Allow-Origin", "*")
+    response.headers.add("Set-Cookie", f"my_cookie='a cookie'; Secure; SameSite=None;")
+    # response.headers.add("Access-Control-Allow-Origin", "http://localhost:3000")
     g.db = models.DATABASE
     g.db.close()
     return response
 
-
-app.register_blueprint(person, url_prefix='/api/v1/user')
-app.register_blueprint(note, url_prefix='/api/v1/note')
-app.register_blueprint(event, url_prefix='/api/v1/event')
-CORS(person)
-CORS(event)
-CORS(note)
-
 @app.route('/')
+@cross_origin()
 def hello_world():
     return "Hello, this flask app is working!!!"
 
